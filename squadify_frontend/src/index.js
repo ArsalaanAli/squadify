6b901e2eba7497743ed5cdbc221e79618f4908b3
Arsalaan Ali <arsalaanali2@gmail.com>
2022-01-22 18:41:45 -0500

    organizing backend

--- squadify_frontend/src/index.js
+++ squadify_frontend/src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from "./App";
+import App from "./App";
 import HomePage from "./HomePage";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HomePage />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
