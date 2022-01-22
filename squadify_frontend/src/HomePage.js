7cd46c5a7e5583f49657c75da7ef18ccbd562942
Arsalaan Ali <arsalaanali2@gmail.com>
2022-01-22 18:35:49 -0500

    fronten: Homepage work

---
+++ squadify_frontend/src/HomePage.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./HomePage.css";
+function HomePage() {
+  return (
+    <div>
+      <h1 className="title">Squadify</h1>
+      <section className="blue">
+        <h1>Title</h1>
+        <p>random text</p>
+      </section>
+      <section className="red">
+        <h1>Title</h1>
+        <p>random text</p>
+      </section>
+      <section className="yellow">
+        <h1>Title</h1>
+        <p>random text</p>
+      </section>
+    </div>
+  );
+}
+
+export default HomePage;
